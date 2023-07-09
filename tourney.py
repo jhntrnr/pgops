1e2f6835f1bd285abee3f8f8ccc0d6c70e1d1d3e
jhntrnr <90057903+jhntrnr@users.noreply.github.com>
2023-07-09 13:28:12 -0500

    Load bots from directory
    Rather than manually importing and instantiating bots, load them from a directory instead.

--- tourney.py
+++ tourney.py
@@ -1,36 +1,62 @@
-from pgops.bots.examples.random_player import RandomPlayer
-from pgops.bots.examples.matching_player import MatchingPlayer
-from pgops.bots.examples.memory_plus import MemoryPlus
-from pgops.bots.examples.adaptive_gops import AdaptiveGops
-from pgops.bots.examples.odd_even import OddEvenBot
-from pgops.bots.examples.match_plus_random import MatchPlusRandom
-from pgops.bots.examples.match_plus_cycle import MatchPlusCycle
-from pgops.bots.examples.my_least_common import MyLeastCommon
-from pgops.bots.examples.adaptive_gops_v2 import AdaptiveGopsV2
+import os
+import importlib
+import inspect
+import argparse
 
+from pgops.bots.pgops_bot import PgopsBot
 from pgops.matchmaking.pgops_orchestrator import Orchestrator
 
+def load_bots(bot_dir, game_type):
+    """Load all PgopsBot classes from python scripts in a specific directory.
+    Excludes bot_template and human_player."""
+    bots = []
+
+    bot_files = [f[:-3] for f in os.listdir(bot_dir) if f.endswith('.py')]
+    for bot_file in bot_files:
+        if bot_file == "__init__" or bot_file == "bot_template" or bot_file == "human_player":
+            continue
+        
+        try:
+            module = importlib.import_module(f'pgops.bots.examples.{bot_file}')
+        except ModuleNotFoundError:
+            print(f'Could not import module {bot_file}')
+            continue
+        
+        classes = inspect.getmembers(module, inspect.isclass)
+
+        for name, obj in classes:
+            if issubclass(obj, PgopsBot) and obj is not PgopsBot:
+                bots.append(obj(game_type))
+                print(f'loaded bot from file {bot_file}')
+
+    return bots
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Run a bot tournament')
+    parser.add_argument('-b', '--bot_dir', default='./pgops/bots/examples', 
+                        help='Path to directory containing bot scripts')
+    parser.add_argument('-v', '--game_type', default='gops', 
+                        choices=['gops', 'bgops', 'bgops_minus'],
+                        help='Type of the game to be played')
+    parser.add_argument('-m', '--matches', type=int, default=4,
+                        help='Number of matches per pairing per round')
+    parser.add_argument('-g', '--games', type=int, default=1000,
+                        help='Number of games per match')
+    parser.add_argument('-t', '--tournaments', type=int, default=3,
+                        help='Number of tournaments to run')
+
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    '''
-    This script will run a round robin tournament between players in `player_pool`.
-    Currently only "round_robin" tournaments are supported.
-    Any number of bots can be instantiated and included in the `player_pool` list.
-    Each pairing will play `matches_per_pairing` matches of `games_per_match` games.
+    args = get_args()
 
-    To force bots to play against themselves, create multiple instances of the bot and include them all in `player_pool`
-    '''
-
-    game_type = "gops"
-    
-    a = RandomPlayer(game_type)
-    b = MatchingPlayer(game_type)
-    c = AdaptiveGops(game_type)
-    d = MemoryPlus(game_type)
-    e = OddEvenBot(game_type)
-    f = MatchPlusRandom(game_type)
-    g = MatchPlusCycle(game_type)
-    h = MyLeastCommon(game_type)
-    i = AdaptiveGopsV2(game_type)
-
-    player_pool = [c,i]
-    Orchestrator(game_type=game_type, player_pool=player_pool, tournament_style="round_robin",matches_per_pairing=4, games_per_match=1000, num_tournaments=3)
+    try:
+        player_pool = load_bots(args.bot_dir, args.game_type)
+        tournament = Orchestrator(game_type=args.game_type,
+                                  player_pool=player_pool,
+                                  tournament_style="round_robin",
+                                  matches_per_pairing=args.matches,
+                                  games_per_match=args.games,
+                                  num_tournaments=args.tournaments)
+    except Exception as e:
+        print(f"An error occurred: {e}")
